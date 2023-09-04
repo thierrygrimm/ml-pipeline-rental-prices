8a75a0eb9f12690285e2fe44c29ebfcf31c6db2e
Thierry <thierry.grimm@hotmail.ch>
2023-09-04 11:10:20 +0200

    refactor: Combine renamed components
    Combine renamed components and fixes relatives path problem

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ import mlflow
 from omegaconf import DictConfig
 
 _steps = [
-    "download",
-    "basic_cleaning",
+    "data_download",
+    "data_clean",
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-    # "test_regression_model"
+    # Not included so it is not run by mistake.
+    # "test_model"
 ]
 
 
@@ -30,14 +28,16 @@ def go(config: DictConfig):
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
+    # Define original root directory
+    root = hydra.utils.get_original_cwd()
+
     # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
-        if "download" in active_steps:
+        if "data_download" in active_steps:
             # Download file and load in W&B
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
+                os.path.join(root, "components", "data_download"),
                 "main",
-                version='main',
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
@@ -46,10 +46,10 @@ def go(config: DictConfig):
                 },
             )
 
-        if "basic_cleaning" in active_steps:
+        if "data_clean" in active_steps:
             _ = mlflow.run(
                 # Hydra executes the script in a different directory than the root
-                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                os.path.join(root, "components", "data_clean"),
                 "main",
                 parameters={
                     "input_artifact": "sample.csv:latest",
@@ -64,7 +64,7 @@ def go(config: DictConfig):
         if "data_check" in active_steps:
             _ = mlflow.run(
                 # Hydra executes the script in a different directory than the root
-                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                os.path.join(root, "components", "data_check"),
                 "main",
                 parameters={
                     "csv": "clean_sample.csv:latest",
@@ -77,7 +77,7 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/train_val_test_split",
+                os.path.join(root, "components", "data_split"),
                 "main",
                 parameters={
                     "input": "clean_sample.csv:latest",
@@ -88,16 +88,13 @@ def go(config: DictConfig):
             )
 
         if "train_random_forest" in active_steps:
-            # NOTE: we need to serialize the random forest configuration into JSON
+            # Serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-            print(os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"))
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                os.path.join(root, "components", "train_random_forest"),
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
@@ -110,15 +107,16 @@ def go(config: DictConfig):
                 },
             )
 
-        if "test_regression_model" in active_steps:
+        if "test_model" in active_steps:
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                os.path.join(root, "components", "test_model"),
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
                     "test_dataset": "test_data.csv:latest"
                 },
             )
+
 
 
 if __name__ == "__main__":
