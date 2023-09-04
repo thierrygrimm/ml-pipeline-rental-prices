bbe1792a889335030f347d57f58905fdd5bba3f8
Thierry <thierry.grimm@hotmail.ch>
2023-09-04 10:59:05 +0200

    refactor: Removed wandb utils dependency

---
+++ components/data_split/run.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""
+This script splits the provided dataframe in test and remainder
+"""
+import argparse
+import logging
+import pandas as pd
+import wandb
+import tempfile
+from sklearn.model_selection import train_test_split
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="data_split")
+    run.config.update(args)
+
+    # Download input artifact. This will also note that this script is using this
+    # particular version of the artifact
+    logger.info(f"Fetching artifact {args.input}")
+    artifact_local_path = run.use_artifact(args.input).file()
+
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("Splitting train_val and test")
+    trainval, test = train_test_split(
+        df,
+        test_size=args.test_size,
+        random_state=args.random_seed,
+        stratify=df[args.stratify_by] if args.stratify_by != 'none' else None,
+    )
+
+    # Save to output files
+    for df, k in zip([trainval, test], ['trainval', 'test']):
+        logger.info(f"Uploading {k}_data.csv dataset")
+        with tempfile.NamedTemporaryFile("w") as fp:
+            # Export dataframe to csv
+            df.to_csv(fp.name, index=False)
+
+            # Log to W&B
+            artifact = wandb.Artifact(
+                name=f"{k}_data.csv",
+                type=f"{k}_data",
+                description=f"{k} split of dataset",
+            )
+
+            # Log file
+            artifact.add_file(fp.name)
+            run.log_artifact(artifact)
+
+            # Wait until the artifact is loaded into W&B and a version is assigned
+            artifact.wait()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Split test and remainder")
+
+    parser.add_argument("input", type=str, help="Input artifact to split")
+
+    parser.add_argument(
+        "test_size", type=float, help="Size of the test split. Fraction of the dataset, or number of items"
+    )
+
+    parser.add_argument(
+        "--random_seed", type=int, help="Seed for random number generator", default=42, required=False
+    )
+
+    parser.add_argument(
+        "--stratify_by", type=str, help="Column to use for stratification", default='none', required=False
+    )
+
+    args = parser.parse_args()
+
+    go(args)
